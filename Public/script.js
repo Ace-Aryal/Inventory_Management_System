8c88ff210240efcbfb62e648336cc2c4d6a9695b
Ace-Aryal <aryaldeelep@gmail.com>
2025-01-15 20:46:16 +0545

    update

---
+++ Public/script.js
@@ -0,0 +1,28 @@
+const myForm = document.querySelector ("form")
+let patreaonsData = []
+
+myForm.addEventListener("submit",e=> {
+  e.preventDefault()
+  const patreonID = myForm.querySelector("#SN").value
+  const patreonName = myForm.querySelector("#NOS").value
+  const titleOfBook = myForm.querySelector("#TOB").value
+  const borrowedDate = myForm.querySelector("#DOB").value
+  console.log(patreonID , borrowedDate , titleOfBook , patreonName)
+  const patreonData = {
+    id: patreonID,
+    name: patreonName,
+    titleOfBook,
+    borrowedDate
+  }
+  
+  patreaonsData.push(patreonData)
+  
+  export default patreaonsData
+  
+  
+  myForm.querySelector("#SN").value = ""
+  myForm.querySelector("#NOS").value = ""
+  myForm.querySelector("#TOB").value = ""
+  
+  alert("Record Added Sucessfully")
+})
